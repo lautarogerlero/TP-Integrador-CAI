e77bf58d91833f55c5318b04cf7c236045c2a057
lautaro-iriazabal <143225494+lautaro-iriazabal@users.noreply.github.com>
2023-10-19 23:41:36 -0300

    Update Proovedor.cs

--- Negocio/Proovedor.cs
+++ Negocio/Proovedor.cs
@@ -1,6 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Modelo;
+using Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace 
 
