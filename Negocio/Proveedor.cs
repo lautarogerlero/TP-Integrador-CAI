66f8a0adceb2063b6fea8fb128c598f2b80c67f5
lautarogerlero <lautarogerlero@gmail.com>
2023-10-20 12:37:15 -0300

    Proveedores

---
+++ Negocio/Proveedor.cs
@@ -0,0 +1,49 @@
+﻿using Modelo;
+using Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace Negocio
+{
+    public class Proveedor
+    {
+        // Lista de proveedores registrados
+        static List<ProveedorModel> proveedores;
+
+        public Proveedor()
+        {
+            // Inicializar la lista de proveedores
+            proveedores = new List<ProveedorModel>();
+        }
+
+        // Método para registrar un nuevo proveedor
+        public static void RegistrarProveedor(string nombre, string apellido, List<int> categoriasProductos)
+        {
+            // Crear un nuevo proveedor con un ID único, el nombre, el apellido, el estado "Activo" y las categorías de productos
+            ProveedorModel nuevoProveedor = new ProveedorModel(Guid.NewGuid(), nombre, apellido, "Activo", categoriasProductos);
+
+            // Agregar el nuevo proveedor a la lista de proveedores
+            proveedores.Add(nuevoProveedor);
+        }
+
+        // Método para dar de baja un proveedor existente
+        public static void DarDeBajaProveedor(string nombre, string apellido)
+        {
+            // Buscar el proveedor
+            ProveedorModel proveedor = proveedores.Find(p => p.Nombre == nombre && p.Apellido == apellido);
+
+            // Si el proveedor existe y está activo, cambiar su estado a "Inactivo"
+            if (proveedor != null && proveedor.Estado == "Activo")
+            {
+                proveedor.Estado = "Inactivo";
+            }
+            else
+            {
+                Console.WriteLine("El proveedor no existe o ya se encuentra Inactivo");
+            }
+        }
+    }
+}
