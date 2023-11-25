32bff40b2bba28e20782a6d05357ce7141c4d131
lautarogerlero <lautarogerlero@gmail.com>
2023-11-24 22:14:10 -0300

    Diagramas

---
+++ Formularios/Form1.cs
@@ -0,0 +1,20 @@
+namespace Formularios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIn(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
