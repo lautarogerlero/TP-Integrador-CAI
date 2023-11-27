155d5e0e3f9aaacc8e3fc7dca87382d0cbdc75ff
lautarogerlero <lautarogerlero@gmail.com>
2023-11-26 22:20:05 -0300

    Forms

--- Formularios/Form1.cs
+++ Formularios/Form1.cs
@@ -1,3 +1,7 @@
+using Modelo;
+using Negocio;
+using Utils;
+
 namespace Formularios
 {
     public partial class Form1 : Form
@@ -9,7 +13,55 @@ namespace Formularios
 
         private void LogIn(object sender, EventArgs e)
         {
+            string nombreUsuario = textBox1.Text;
+            string contraseña = textBox2.Text;
 
+            Login login = new Login();
+            Dictionary<string, int> intentosPorUsuario = new Dictionary<string, int>();
+            string idUsuario = "";
+            
+            login.NombreUsuario = nombreUsuario;
+            login.Contraseña = contraseña;
+
+            try
+            {
+                idUsuario = Usuario.LogIn(login, nombreUsuario, contraseña);
+                MessageBox.Show("Login exitoso");
+                // Si el inicio de sesión es exitoso, restablece el contador de intentos fallidos.
+                intentosPorUsuario.Remove(login.NombreUsuario);
+
+                // Crea una instancia de Form2 pasando el valor de idUsuario al constructor
+                FrmVendedor frmVendedor = new FrmVendedor();
+
+                // Oculta el formulario actual
+                this.Hide();
+
+                // Muestra el nuevo formulario
+                frmVendedor.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos");
+                // Incrementa el contador de intentos fallidos para este NombreUsuario.
+                if (intentosPorUsuario.ContainsKey(login.NombreUsuario))
+                {
+                    intentosPorUsuario[login.NombreUsuario]++;
+                }
+                else
+                {
+                    intentosPorUsuario[login.NombreUsuario] = 1;
+                }
+                Console.WriteLine($"Le queda/n {3 - intentosPorUsuario[login.NombreUsuario]} intento/s");
+                // Verifica si el usuario ha superado el número máximo de intentos permitidos. Si lo superó, borra al usuario
+                if (intentosPorUsuario[login.NombreUsuario] >= 3)
+                {
+                    MessageBox.Show("Alcanzó el límite de intentos. Su usuario ha sido bloqueado");
+                    //string idABorrar = ObtenerIdUsuario(login.NombreUsuario);
+                    //BorrarUsuario(idABorrar);
+                    //nombreUsuario = "";
+                    //break;
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
---
+++ Formularios/FrmVendedor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formularios
+{
+    public partial class FrmVendedor : Form
+    {
+        public FrmVendedor()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmVendedor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
